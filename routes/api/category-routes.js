1276250e8239f5bdb8f3e034b21e332ec809aaea
Allex Ortiz <allexortiz@outlook.com>
2023-12-03 21:10:31 -0600

    add API to find category by its id value and added API route to handle POST requests

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -13,19 +13,40 @@ router.get('/', async (req, res) => {
       attributes: ["id", "product_name", "price", "stock", "category_id"]
     }]
   })
-  .then((categories) => {
-    res.json(categories);
-  })
+    .then((categories) => {
+      res.json(categories);
+    })
 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  await Category.findByPk(req.params.id, {
+    attributes: ["id", "category_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      }
+    ],
+  })
+    .then((category) => {
+      res.json(category);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  await Category.create(req.body)
+		.then((newCategory) => res.status(200).json(newCategory))
+		.catch((err) => {
+			console.log(err);
+			res.status(400).json(err);
+		});
 });
 
 router.put('/:id', (req, res) => {
@@ -34,17 +55,17 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
-	await Category.destroy({
-		where: {
-			id: req.params.id,
-		},
-	})
-	.then((rmvdCategory) => {
-		res.json(`The category was removed from the database`);
-	})
-	.catch((err) => {
-		res.json(err);
-	});
+  await Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((rmvdCategory) => {
+      res.json(`The category was removed from the database`);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 module.exports = router;
